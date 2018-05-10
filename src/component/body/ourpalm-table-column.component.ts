59fc0e44f1d507c377ecec98af395ef347c189de
lishichao <lishichao1002@163.com>
2018-05-10 10:16:45 +0800

    add headerTpl

--- src/component/body/ourpalm-table-column.component.ts
+++ src/component/body/ourpalm-table-column.component.ts
@@ -1,15 +1,23 @@
-import {Component, ContentChild, Input, TemplateRef} from "@angular/core";
+import {AfterViewInit, Component, ContentChild, Input, TemplateRef} from "@angular/core";
 import {OurpalmTableColumn} from "../../model/ourpalm-table-column";
 
 @Component({
     selector: 'ourpalm-table-column',
     template: ` `
 })
-export class OurpalmTableColumnComponent {
+export class OurpalmTableColumnComponent implements AfterViewInit {
 
     @Input()
     column: OurpalmTableColumn;
 
     @ContentChild(TemplateRef)
     template: TemplateRef<any>;
+
+    @ContentChild('row', {read: TemplateRef})
+    rowTemplate: TemplateRef<any>;
+
+    ngAfterViewInit() {
+        this.template = this.rowTemplate || this.template;
+    }
+
 }
--- src/component/header/ourpalm-table-header-cell.component.ts
+++ src/component/header/ourpalm-table-header-cell.component.ts
@@ -8,13 +8,23 @@ import {OurpalmTableColumn} from "../../model/ourpalm-table-column";
     template: `
         <!-- 排序列 -->
         <span *ngIf="column.sort" (click)="onSortColumn.next(column)" [class.sortable]="column.sort">
-                {{column.header}}
+                <ng-container *ngIf="!isTemplate">
+                    {{column.header}}
+                </ng-container>
+                <ng-container *ngIf="isTemplate">
+                    <ng-template [ngTemplateOutlet]="column.headerTpl"></ng-template>
+                </ng-container>
                 <i class="fa"
                    [ngClass]="{'fa-sort-asc': column.sortOrder == 'asc', 'fa-sort-desc': column.sortOrder == 'desc', 'fa-sort': !column.sortOrder}"></i>
         </span>
         <!-- 正常列 | 序号列-->
         <ng-container *ngIf="!column.sort && !column.checkbox">
-            {{column.header}}
+            <ng-container *ngIf="!isTemplate">
+                {{column.header}}
+            </ng-container>
+            <ng-container *ngIf="isTemplate">
+                <ng-template [ngTemplateOutlet]="column.headerTpl"></ng-template>
+            </ng-container>
         </ng-container>
     `
 })
@@ -22,4 +32,9 @@ export class OurpalmTableHeaderCellComponent {
 
     @Input() column: OurpalmTableColumn;
     @Output() onSortColumn: EventEmitter<OurpalmTableColumn> = new EventEmitter();
+
+    get isTemplate(): boolean {
+        return this.column ? (typeof this.column.header === 'string') : false;
+    }
+
 }
